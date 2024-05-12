49fdb7246afed0393f1aa85b1934ef175813b035
tukamartin <tuka.martin@gmail.com>
2024-05-12 20:10:58 +0200

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+/*viewportWidth: 1920,
+  viewportHeight: 1080,
+  defaultCommandTimeout: 50000,
+  requestTimeout: 500000,
+  responseTimeout: 500000,
+  uncaught: 'exception',
+  chromeWebSecurity: false,
+  numTestsKeptInMemory: 0,
+*/
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  }
+}
